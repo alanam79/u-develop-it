c5fd5cf1e72aab46aa0f93c8fe4c880b67bd90b8
alanam79 <halo2382001@gmail.com>
2022-06-11 14:17:19 -0400

    tested ojbect being pulled into object

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const mysql = require('mysql2');
+const express = require("express");
+const mysql = require("mysql2");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,33 +8,31 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// connect to mysql database
+// Connect to database
 const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        // your mysql username,
-        user: 'root',
-        // your mysql password
-        password: 'Mae,hN2Ab-O-',
-        database: 'election'
-    },
-    console.log('Connected to the election database!❤️')
-)
+  {
+    host: "localhost",
+    // Your MySQL username,
+    user: "root",
+    // your mysql password
+    password: "Mae,hN2Ab-O-",
+    database: "election",
+  },
+  console.log("Connected to the election database!❤️")
+);
 
-
-
-
-
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
 
 
 
 // Default response for any other request (not found), this is a very important catchall, so must be the last route
 app.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
-
 
 // starts server
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}🚀`)
-}); 
+  console.log(`Server running on port ${PORT}🚀`);
+});
