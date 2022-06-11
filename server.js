a92e8c03afae9bd962bff2313de404d837d36d4d
alanam79 <halo2382001@gmail.com>
2022-06-11 14:10:25 -0400

    conected mysql to expres

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mysql = require('mysql2');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -6,18 +8,30 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// connect to mysql database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // your mysql username,
+        user: 'root',
+        // your mysql password
+        password: 'Mae,hN2Ab-O-',
+        database: 'election'
+    },
+    console.log('Connected to the election database!❤️')
+)
 
-app.get('/', (req, res) => {
-    res.json({
-        message: "Hello World!"
-    });
-});
 
-// Default response for any other request (not fouond)
+
+
+
+
+
+
+// Default response for any other request (not found), this is a very important catchall, so must be the last route
 app.use((req, res) => {
     res.status(404).end();
 });
-
 
 
 // starts server
